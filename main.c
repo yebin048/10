517b969a009b6fce88fff2bcd92fd64b7a860ef6
yebin048 <lyna0408@naver.com>
2025-11-07 17:30:47 +0900

    4번 실습

--- main.c
+++ main.c
@@ -4,13 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[]="The worst things to eat before you sleep";
-	char dst[100];
+	char str[30]="Happy C programming";
 	
-	strcpy(dst,src);
-	
-	
-	printf("copied string: %s\n", dst);
+	printf("string %s length : %i\n", str, strlen(str));
 	
 	return 0;
 }
